aea0c6ac487448c181bcd65c9bfcc97f8326c385
Michael <michael-hyde@hotmail.co.uk>
2019-10-29 11:26:41 +0000

    project html

---
+++ KnifeManScript.js
@@ -0,0 +1,13 @@
+
+function selectTimeSlot(ele) {
+    
+    document.getElementById("time").innerHTML = ele.value
+
+}
+function selectDate(ele) {
+   
+    document.getElementById("date").innerHTML = ele.value
+
+}
+
+
